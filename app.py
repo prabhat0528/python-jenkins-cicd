497240f8d8c0abc8f1c8e4c9acbeafaad05b9a02
prabhat0528 <prabhatrai0528@gmail.com>
2026-05-14 12:36:01 +0530

    Triggering the pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from home route."
+    return "Hello from home route. Nice"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port = 4000)
